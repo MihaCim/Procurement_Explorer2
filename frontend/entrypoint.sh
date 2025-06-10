83fb9f23e6ba16a85c7aee29c65e286e4bd4b26e
zanjonke <zj@munich-innovation.com>
2025-06-10 13:54:36 +0200

    Fix for frontend

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Substitute environment variables in the Nginx configuration template
+envsubst '${VITE_API_BASE_URL} ${VITE_API_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
 # Start Nginx
 nginx -g 'daemon off;'
