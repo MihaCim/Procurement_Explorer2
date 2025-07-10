f1d77da72c03c56489f09debccbf05b0b475277b
Miha Cimperman <miha.cimperman@gmail.com>
2025-07-10 12:26:28 +0200

    roll back multithread to 4 workers

--- DueDilligence/entrypoint.sh
+++ DueDilligence/entrypoint.sh
@@ -5,5 +5,5 @@ if [ "$APP_MODE" = "streamlit" ]; then
     exec streamlit run streamlit_app.py --server.port=8501 --server.address=0.0.0.0
 else
     echo "Starting in Uvicorn mode..."
-    exec uvicorn run:app --host 0.0.0.0 --port 8501 #--workers 4
+    exec uvicorn run:app --host 0.0.0.0 --port 8501 --workers 4
 fi
